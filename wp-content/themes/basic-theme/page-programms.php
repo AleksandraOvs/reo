a7e470083887919e3ff34597a0d55c1775cdb74b
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-05-20 14:41:06 +0300

    eds

--- wp-content/themes/basic-theme/page-programms.php
+++ wp-content/themes/basic-theme/page-programms.php
@@ -17,9 +17,9 @@ get_header(); ?>
        
 <section class="site-page" <?php //if (is_active_sidebar('page-sidebar1')) : echo 'style="width:73%;"'; endif;
                             ?>>
-     <div class="page-header__inner"></div>
+     <div class="page-header__inner "></div>
    
-    <div class="fixed-container">
+    <div class="fixed-container programms">
         <?php get_template_part('templates/programms'); ?>
     </div>
 
