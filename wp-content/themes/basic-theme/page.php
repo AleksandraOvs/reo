2f70099deccf6ec33514a75ce313b55f93a9f349
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-05-20 14:09:22 +0300

    eds

--- wp-content/themes/basic-theme/page.php
+++ wp-content/themes/basic-theme/page.php
@@ -16,21 +16,49 @@ get_header(); ?>
 <div class="page-container">
 
        
-    <section class="site-page" <?php //if (is_active_sidebar('page-sidebar1')) : echo 'style="width:73%;"'; endif;
+    <section class="site-page" style="padding-top:140px;" <?php //if (is_active_sidebar('page-sidebar1')) : echo 'style="width:73%;"'; endif;
                                 ?>>
-         <div class="page-header__inner">
-         <?php
-        // if (has_post_thumbnail()) { // условие, если есть миниатюра
-        //     the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
-        // } 
-        ?>
+         <!-- <div class="page-header__inner">
             <div class="fixed-container">
-                <h2 class="site-page__title toopacity white"> <?php the_title(); ?> </h2>
+                <h2 class="site-page__title toopacity white"> <?php //the_title(); ?> </h2>
             </div>
+        </div> -->
+        <div class="fixed-container">
+        <?php if (is_search() ): ?>
+                
+                    <h1>Результаты поиска по запросу: &laquo;<?php the_search_query()?>&raquo;</h1>
+              
+            <?php endif; ?>
+
+            <?php if(have_posts()) : ?>
+              <div class="posts__grid">
+                <?php 
+              while(have_posts() ) : the_post();
+
+                    get_template_part('templates/entry-content');
+
+            endwhile;
+            ?>
+              </div>
+              
+             
+                <div class="pagination">
+                  <?php
+                      echo paginate_links(array(
+                          'prev_next' => true,
+                          'prev_text' => __('<i class="icon arrow_carrot-left"></i>>'),
+                          'next_text' => __('<i class="icon arrow_carrot-right"></i>>'),
+                      ));
+                  ?>
+
+                </div>
+              
+                    <?php else: ?>
+                        <p>По вашему запросу ничего не найдено</p>
+                      <?php endif; ?>
         </div>
-        <div class="full-width-container">
-       <?php the_content() ?>
-        </div>
+
+
     </section>
  
 </div>
