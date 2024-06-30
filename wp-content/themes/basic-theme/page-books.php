4ad7da729732d7c23c85fc812e0d13fcbacc421b
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-06-30 17:46:09 +0300

    ds

--- wp-content/themes/basic-theme/page-books.php
+++ wp-content/themes/basic-theme/page-books.php
@@ -19,9 +19,9 @@ get_header(); ?>
                                 ?>>
         <div class="page-header__inner">
         <?php
-        if (has_post_thumbnail()) { // условие, если есть миниатюра
-            the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
-        } 
+        // if (has_post_thumbnail()) { // условие, если есть миниатюра
+        //     the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
+        // } 
         ?>
             <div class="fixed-container">
                 <h2 class="site-page__title toopacity white"> <?php the_title(); ?> </h2>
