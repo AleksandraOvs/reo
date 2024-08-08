eabba0db0cb69c0f348c67113343fd4f7f0cc9a9
Aleksandraovs <aleksandra.ovsia@gmail.com>
2024-08-08 15:11:41 +0300

    eds

--- wp-content/themes/reocano-theme/page.php
+++ wp-content/themes/reocano-theme/page.php
@@ -15,22 +15,24 @@ get_header(); ?>
 
 <div class="page-container">
 
-
+       
     <section class="site-page" <?php //if (is_active_sidebar('page-sidebar1')) : echo 'style="width:73%;"'; endif;
                                 ?>>
-        <div class="page-header__inner">
-            <?php
-            // if (has_post_thumbnail()) { // условие, если есть миниатюра
-            //     the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
-            // } 
-            ?>
-            
+         <div class="page-header__inner">
+         <?php
+        // if (has_post_thumbnail()) { // условие, если есть миниатюра
+        //     the_post_thumbnail('full'); // если параметры функции не указаны, то выводится миниатюра текущего поста, размер thumbnail
+        // } 
+        ?>
+            <div class="fixed-container">
+                <h2 class="site-page__title toopacity white"> <?php the_title(); ?> </h2>
+            </div> 
         </div>
         <div class="full-width-container">
-            <?php the_content() ?>
+       <?php the_content() ?>
         </div>
     </section>
-
+ 
 </div>
 
 
