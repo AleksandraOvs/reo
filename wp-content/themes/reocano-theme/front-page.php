d1e5bcf30adb1853aea1eab5b5a75aeb96b211eb
aleksandra <aleksandra.ovsia@gmail.com>
2024-10-16 09:19:09 +0300

    add blog description

--- wp-content/themes/reocano-theme/front-page.php
+++ wp-content/themes/reocano-theme/front-page.php
@@ -6,7 +6,8 @@
 
     <!-- <div class="section-programms">
         <div class="fixed-container">
-            <?php //get_template_part('templates/programms') ?>
+            <?php //get_template_part('templates/programms') 
+            ?>
         </div>
     </div> -->
 
@@ -19,9 +20,12 @@
     <div class="section-blog">
         <div class="fixed-container">
             <h2 class="site-page__title">Блог Клуба</h2>
-            <div class="section-blog__desc">
-            Узнайте наши новости, информацию о мероприятиях и других активностях Клуба. Также в Блоге публикуются научно-познавательные статьи руководства Клуба. Они погрузятся в иной, манящий мир шаманизма.
-            </div>
+            <?php
+            if ($cat_desc = category_description(6)){
+                echo '<div class="section-blog__desc">' . $cat_desc . '</div>';
+            }
+            ?>
+            
             <?php
             $query = new WP_Query([
                 'post_type' => 'post',
@@ -30,40 +34,41 @@
 
             ?>
             <div class="swiper posts-slider">
-            <!-- <div class="posts__grid"> -->
-            <div class="swiper-wrapper">
-            <?php
+                <!-- <div class="posts__grid"> -->
+                <div class="swiper-wrapper">
+                    <?php
 
-            while ($query->have_posts()) {
-                $query->the_post();
-            ?>
-                <div class="entry-post__inner swiper-slide">
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                    ?>
+                        <div class="entry-post__inner swiper-slide">
 
-                    <div  class="entry-img">
-                        <?php if (has_post_thumbnail()) {
-                            the_post_thumbnail('full', array('class' => 'destr-img'));
-                        } else {
-                            echo '<img class="destr-img" src="' . get_bloginfo('stylesheet_directory')
-                                . '/images/placeholder.jpg" />';
-                        } ?>
+                            <div class="entry-img">
+                                <?php if (has_post_thumbnail()) {
+                                    the_post_thumbnail('full', array('class' => 'destr-img'));
+                                } else {
+                                    echo '<img class="destr-img" src="' . get_bloginfo('stylesheet_directory')
+                                        . '/images/placeholder.jpg" />';
+                                } ?>
 
-                    </div>
-                    <a href="<?php the_permalink() ?>" class="entry-post__inner__head">
-                        <h3><?php the_title(); ?></h3>
-                        <?php //the_excerpt() ?>
-                        <span>Читать далее</span>
-                    </a>
+                            </div>
+                            <a href="<?php the_permalink() ?>" class="entry-post__inner__head">
+                                <h3><?php the_title(); ?></h3>
+                                <?php //the_excerpt() 
+                                ?>
+                                <span>Читать далее</span>
+                            </a>
+
+                        </div>
+                    <?php
+
+                    }
+
+                    wp_reset_postdata();
+                    ?>
 
                 </div>
-            <?php
-
-            }
-
-            wp_reset_postdata();
-            ?>
-            
-            </div>
-            <div class="posts-slider__pagination"></div>
+                <div class="posts-slider__pagination"></div>
             </div>
         </div>
     </div>
